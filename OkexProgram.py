c9f7aaad661939ecff7eff03e5713b911695ef92
wendongdi <wddiswendd@gmail.com>
2022-04-09 02:25:49 +0800

    测试通过

---
+++ OkexProgram.py
@@ -0,0 +1,29 @@
+import threading
+
+import OkexApp
+import handlers
+
+
+class Market:
+	def __init__(self):
+		# 记录spot tick数据
+		self.spot_tick_line = []
+		self.spot_trade_line = []
+		self.swap_tick_line = []
+		self.swap_trade_line = []
+
+
+if __name__ == '__main__':
+	mkt = Market()
+	# 监听行情
+	handlers.mkt = mkt
+	# 同步锁
+	handlers.books_lock = threading.Lock()
+	handlers.trade_lock = threading.Lock()
+	# 运行websocket
+	ws_thread = OkexApp.start()
+	if ws_thread:
+		try:
+			ws_thread.join()
+		finally:
+			pass
