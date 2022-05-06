263fb3b4d74f774d29eea1a98ef7758fa29ebeed
wendongdi <wddiswendd@gmail.com>
2022-05-07 00:48:21 +0800

    修复深度数据

--- constants.py
+++ constants.py
@@ -43,7 +43,7 @@ class Channel(str):
 	MARK_PRICE = "mark-price"  # 标记价格频道
 	MARK_PRICE_CANDLE1D = "mark-price-candle1D"  # 标记价格K线频道
 	PRICE_LIMIT = "price-limit"  # 限价频道, 获取交易的最高买价和最低卖价
-	BOOKS = "books5"  # 深度频道
+	BOOKS = "books"  # 深度频道
 	BOOKS50 = "books50-l2-tbt"  # 深度频道
 	OPT_SUMMARY = "opt-summary"  # 期权定价频道
 	FUNDING_RATE = "funding-rate"  # 资金费率频道
